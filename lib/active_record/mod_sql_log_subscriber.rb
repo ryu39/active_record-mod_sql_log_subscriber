61b614f3132b8332f97d5d4994ab2118d86d4752
ryu39 <dev.ryu39@gmail.com>
2019-02-02 15:37:22 +0900

    Initial commit

---
+++ lib/active_record/mod_sql_log_subscriber.rb
@@ -0,0 +1,8 @@
+require "active_record/mod_sql_log_subscriber/version"
+
+module ActiveRecord
+  module ModSqlLogSubscriber
+    class Error < StandardError; end
+    # Your code goes here...
+  end
+end
---
+++ lib/active_record/mod_sql_log_subscriber/version.rb
@@ -0,0 +1,5 @@
+module ActiveRecord
+  module ModSqlLogSubscriber
+    VERSION = "0.1.0"
+  end
+end
